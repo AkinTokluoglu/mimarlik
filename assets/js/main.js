c1a313262ecff15c3eca36e9b7580fde2028bcdc
Akın Tokluoğlu <akintokluoglu@gmail.com>
2024-06-23 16:20:51 +0300

    V5

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,3 +167,97 @@
   new PureCounter();
 
 })();
+
+
+// Dinamik HTML oluşturma Mutfak
+const portfolioContainerKitchen = document.getElementById('portfolio-container-kitchen');
+const portfolioItemsKitchen = Array.from({ length: 29 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-kitchen">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/mutfak-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/mutfak-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Mutfak">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerKitchen.innerHTML = portfolioItemsKitchen;
+
+// Dinamik HTML oluşturma Giyinme Odasi
+const portfolioContainerDressing = document.getElementById('portfolio-container-dressing');
+const portfolioItemsDressing = Array.from({ length: 11 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-dressing">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/giyinme-odasi-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/giyinme-odasi-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Giyinme Odasi">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerDressing.innerHTML = portfolioItemsDressing;
+
+
+// Dinamik HTML oluşturma Banyo
+const portfolioContainerBathroom = document.getElementById('portfolio-container-bathroom');
+const portfolioItemsBathroom = Array.from({ length: 4 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-bathroom">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/banyo-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/banyo-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Banyo">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerBathroom.innerHTML = portfolioItemsBathroom;
+
+// Dinamik HTML oluşturma Cafe
+const portfolioContainerCafe = document.getElementById('portfolio-container-cafe');
+const portfolioItemsCafe = Array.from({ length: 8 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-cafe">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/cafe-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/cafe-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Cafe">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerCafe.innerHTML = portfolioItemsCafe;
+
+// Dinamik HTML oluşturma Otel
+const portfolioContainerHotel = document.getElementById('portfolio-container-hotel');
+const portfolioItemsHotel = Array.from({ length: 12 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-hotel">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/otel-odasi-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/otel-odasi-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Otel Odasi">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerHotel.innerHTML = portfolioItemsHotel;
+
+// Dinamik HTML oluşturma TV
+const portfolioContainerTV = document.getElementById('portfolio-container-tv');
+const portfolioItemsTV = Array.from({ length: 10 }, (_, i) => `
+<div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-tv">
+<div class="portfolio-content h-100">
+<a href="assets/img/projects/televizyon-unitesi-${i + 1}.webp" data-gallery="portfolio-gallery-app" class="glightbox preview-link">
+<img src="assets/img/projects/televizyon-unitesi-${i + 1}.webp" class="img-fluid" alt="Kayseri İç Mimar Televizyon Unitesi">
+</a>
+</div>
+</div>
+`).join('');
+
+portfolioContainerTV.innerHTML = portfolioItemsTV;
+
+const lightbox = GLightbox({
+  selector: '.glightbox'
+});
+
+// Dinamik HTML içerik eklendikten sonra yeniden başlat
+portfolioContainerDressing.innerHTML = portfolioItemsDressing;
+lightbox.reload();  // glightbox eklentisini yeniden başlatmak için
